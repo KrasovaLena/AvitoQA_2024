cbc84a0e5534c71249275cafa6de50a593bb4523
Lena Krasova <krasovaem@gmail.com>
2024-04-14 18:09:28 +0500

    Avito trainee assignmemt completed

--- Tests.py
+++ Tests.py
@@ -15,7 +15,7 @@ mock_url = 'https://www.avito.ru/web/1/charity/ecoImpact/init'
 def test_1_negative_values(page):
     page.route(mock_url, lambda route: route.fulfill(json=negative_values_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test1-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test1-water.png')
     page.locator(energy_counter).screenshot(path='./output/test1-energy.png')
@@ -25,7 +25,7 @@ def test_1_negative_values(page):
 def test_2_zero_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=zero_values_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test2-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test2-water.png')
     page.locator(energy_counter).screenshot(path='./output/test2-energy.png')
@@ -35,7 +35,7 @@ def test_2_zero_boundary(page):
 def test_3_positive_units(page):
     page.route(mock_url, lambda route: route.fulfill(json=positive_units_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test3-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test3-water.png')
     page.locator(energy_counter).screenshot(path='./output/test3-energy.png')
@@ -45,7 +45,7 @@ def test_3_positive_units(page):
 def test_4_below_thousand_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=thousand_boundary_below_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test4-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test4-water.png')
     page.locator(energy_counter).screenshot(path='./output/test4-energy.png')
@@ -55,7 +55,7 @@ def test_4_below_thousand_boundary(page):
 def test_5_thousand_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=thousand_boundary_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test5-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test5-water.png')
     page.locator(energy_counter).screenshot(path='./output/test5-energy.png')
@@ -65,7 +65,7 @@ def test_5_thousand_boundary(page):
 def test_6_above_thousand_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=thousand_boundary_above_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test6-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test6-water.png')
     page.locator(energy_counter).screenshot(path='./output/test6-energy.png')
@@ -75,7 +75,7 @@ def test_6_above_thousand_boundary(page):
 def test_7_below_millions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=millions_boundary_below_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test7-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test7-water.png')
     page.locator(energy_counter).screenshot(path='./output/test7-energy.png')
@@ -85,7 +85,7 @@ def test_7_below_millions_boundary(page):
 def test_8_millions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=millions_boundary_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test8-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test8-water.png')
     page.locator(energy_counter).screenshot(path='./output/test8-energy.png')
@@ -95,7 +95,7 @@ def test_8_millions_boundary(page):
 def test_9_above_millions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=millions_boundary_above_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test9-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test9-water.png')
     page.locator(energy_counter).screenshot(path='./output/test9-energy.png')
@@ -105,7 +105,7 @@ def test_9_above_millions_boundary(page):
 def test_10_below_billions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=billions_boundary_below_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test10-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test10-water.png')
     page.locator(energy_counter).screenshot(path='./output/test10-energy.png')
@@ -115,7 +115,7 @@ def test_10_below_billions_boundary(page):
 def test_11_billions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=billions_boundary_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test11-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test11-water.png')
     page.locator(energy_counter).screenshot(path='./output/test11-energy.png')
@@ -125,7 +125,7 @@ def test_11_billions_boundary(page):
 def test_12_above_billions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=billions_boundary_above_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test12-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test12-water.png')
     page.locator(energy_counter).screenshot(path='./output/test12-energy.png')
@@ -135,7 +135,7 @@ def test_12_above_billions_boundary(page):
 def test_13_below_trillions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=trillions_boundary_below_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test13-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test13-water.png')
     page.locator(energy_counter).screenshot(path='./output/test13-energy.png')
@@ -145,7 +145,7 @@ def test_13_below_trillions_boundary(page):
 def test_14_trillions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=trillions_boundary_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test14-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test14-water.png')
     page.locator(energy_counter).screenshot(path='./output/test14-energy.png')
@@ -155,7 +155,7 @@ def test_14_trillions_boundary(page):
 def test_15_above_trillions_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=trillions_boundary_above_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test15-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test15-water.png')
     page.locator(energy_counter).screenshot(path='./output/test15-energy.png')
@@ -165,7 +165,7 @@ def test_15_above_trillions_boundary(page):
 def test_16_below_max_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=below_max_value_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test16-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test16-water.png')
     page.locator(energy_counter).screenshot(path='./output/test16-energy.png')
@@ -175,7 +175,7 @@ def test_16_below_max_boundary(page):
 def test_17_max_boundary(page):
     page.route(mock_url, lambda route: route.fulfill(json=max_value_json))
     page.goto(main_url)
-    time.sleep(3)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test17-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test17-water.png')
     page.locator(energy_counter).screenshot(path='./output/test17-energy.png')
@@ -185,7 +185,7 @@ def test_17_max_boundary(page):
 def test_18_negative_testing(page):
     page.route(mock_url, lambda route: route.fulfill(json=insane_json))
     page.goto(main_url)
-    time.sleep(5)
+    time.sleep(4)
     page.locator(co2_counter).screenshot(path='./output/test18-CO2.png')
     page.locator(water_counter).screenshot(path='./output/test18-water.png')
     page.locator(energy_counter).screenshot(path='./output/test18-energy.png')
